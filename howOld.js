8bb2c59eba19903d2f09784e329a9f7fce243ff5
Fabritscio <86211660+Fabritscio@users.noreply.github.com>
2022-05-07 19:42:51 +0100

    Add files via upload

---
+++ howOld.js
@@ -0,0 +1,13 @@
+// Write your function here:
+const howOld = (age, year) => {
+  
+    date.time = year
+    if (year === 'future') {
+      return `You will be ${age} in the year ${year}`
+    } else if (year === 'before') {
+      return `The year ${year} was ${year} years before you were born`;
+    } else if (year === 'past') {
+      return `The year ${age} in the year ${year}`;
+    }
+  }
+  
